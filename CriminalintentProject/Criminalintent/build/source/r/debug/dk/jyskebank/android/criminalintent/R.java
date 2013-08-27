1a4a95b46068cffd1cbe04d0f0c42696f5f02d89
Peter Bødskov <peter.boedskov@jyskebank.dk>
2013-08-27 21:22:05 +0200

    chapter 8 done

--- CriminalintentProject/Criminalintent/build/source/r/debug/dk/jyskebank/android/criminalintent/R.java
+++ CriminalintentProject/Criminalintent/build/source/r/debug/dk/jyskebank/android/criminalintent/R.java
@@ -21,6 +21,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int crime_date=0x7f070002;
+        public static final int crime_solved=0x7f070003;
         public static final int crime_title=0x7f070001;
         public static final int fragmentContainer=0x7f070000;
     }
@@ -32,7 +34,10 @@ public final class R {
         /**  From: file:/Users/jb3579/Documents/Programming/Java/Android/workspace/criminalintent/CriminalintentProject/Criminalintent/src/main/res/values/strings.xml 
          */
         public static final int app_name=0x7f050000;
-        public static final int crime_title_hint=0x7f050001;
+        public static final int crime_details_label=0x7f050001;
+        public static final int crime_solved_label=0x7f050002;
+        public static final int crime_title_hint=0x7f050003;
+        public static final int crime_title_label=0x7f050004;
     }
     public static final class style {
         /**  From: file:/Users/jb3579/Documents/Programming/Java/Android/workspace/criminalintent/CriminalintentProject/Criminalintent/src/main/res/values/styles.xml 
